6140df8fd45e21afd107e1ead6f264922d511a90
shaddu <Shad@Shadabs-MacBook-Air.local>
2017-01-29 20:58:26 +0530

    model

--- elasto/apps/core/models.py
+++ elasto/apps/core/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.core.validators import MinLengthValidator,  MaxLengthValidator
 
-# Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Student(models.Model):
+    YEAR_IN_SCHOOL_CHOICES = (
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+    )
+    year_in_school = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
+    age = models.SmallIntegerField(validators=[MinLengthValidator(1),, MaxLengthValidator(100)])
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # relationships models
+    university = models.ForeignKey(University, null=True, blank=True)
+    courses = models.ManyToManyField(Course, null=True, blank=True)
+
+
+
