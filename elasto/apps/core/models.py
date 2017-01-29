d3a36c18733b58f0eae932f22609191c292ac30b
shaddu <Shad@Shadabs-MacBook-Air.local>
2017-01-30 00:42:25 +0530

    adding es mapping

--- elasto/apps/core/models.py
+++ elasto/apps/core/models.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.core.validators import MinLengthValidator,  MaxLengthValidator
-
+import django.db.models.options as options
+options.DEFAULT_NAMES = options.DEFAULT_NAMES + (
+    'es_index_name', 'es_type_name', 'es_mapping'
+)
 class University(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -9,6 +12,34 @@ class Course(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
 class Student(models.Model):
+    class Meta:
+        es_index_name = 'django'
+        es_type_name = 'student'
+        es_mapping = {
+            'properties': {
+                'university': {
+                    'type': 'object',
+                    'properties': {
+                        'name': {'type': 'string', 'index': 'not_analyzed'},
+                    }
+                },
+                'first_name': {'type': 'string', 'index': 'not_analyzed'},
+                'last_name': {'type': 'string', 'index': 'not_analyzed'},
+                'age': {'type': 'short'},
+                'year_in_school': {'type': 'string'},
+                'name_complete': {
+                    'type': 'completion',
+                    'analyzer': 'simple',
+                    'payloads': True,
+                    'preserve_separators': True,
+                    'preserve_position_increments': True,
+                    'max_input_length': 50,
+                },
+                "course_names": {
+                    "type": "string", "store": "yes", "index": "not_analyzed",
+                },
+            }
+        }
     YEAR_IN_SCHOOL_CHOICES = (
         ('FR', 'Freshman'),
         ('SO', 'Sophomore'),
